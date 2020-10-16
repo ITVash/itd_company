d1a6a5b65deaff66299e047cbc183e443b757b99
ITVash <vash_dn@mail.ru>
2020-10-16 16:32:06 +0300

    download

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
-import ReadMore from 'read-more-react'
+import ReadMore from "read-more-react"
 
 import { Header, Footer } from "../../Component"
-import { Forms } from '../../Container'
-import { Clickbait } from '../../Component'
+import { Forms } from "../../Container"
+import { Clickbait } from "../../Component"
 
 import "./style.scss"
 
@@ -91,16 +91,16 @@ const Home = () => {
 		<>
 			<Helmet>
 				<title>ITD Company - Главная</title>
-				<meta name='description' content='Главная страница сайта' />
+				<meta name="description" content="Главная страница сайта" />
 			</Helmet>
 			<Header />
-			<section className='index_main_screen'>
-				<div className='wrapper'>
-					<div className='index_main_screen_content_container'>
-						<div className='index_main_screen_logo_container'>
-							<img src='/img/logo_172.png' alt='logo' />
+			<section className="index_main_screen">
+				<div className="wrapper">
+					<div className="index_main_screen_content_container">
+						<div className="index_main_screen_logo_container">
+							<img src="/img/logo_172.png" alt="logo" />
 						</div>
-						<div className='index_main_screen_name_container'>
+						<div className="index_main_screen_name_container">
 							<h1>
 								international
 								<br />
@@ -109,21 +109,26 @@ const Home = () => {
 								dynamics
 							</h1>
 							<h2>Команда специалистов, которая впечатляет!</h2>
-							<div className='index_main_screen_text'>
+							<div className="index_main_screen_text">
 								Ответьте всего на 5 вопросов
 								<br />и узнайте цену Вашего сайта
 							</div>
-							<button className='main_screen_button' onClick={()=> setClick(true)}>Узнать цену</button>
+							<button
+								className="main_screen_button"
+								onClick={() => setClick(true)}
+							>
+								Узнать цену
+							</button>
 						</div>
-						<div className='index_main_screen_img_container'>
-							<img src='/img/index_main_img.png' alt='img' />
+						<div className="index_main_screen_img_container">
+							<img src="/img/index_main_img.png" alt="img" />
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className='index_advantages_screen'>
-				<div className='wrapper'>
-					<div className='gradient_title'>
+			<section className="index_advantages_screen">
+				<div className="wrapper">
+					<div className="gradient_title">
 						<h2>
 							Наши
 							<br />
@@ -131,101 +136,107 @@ const Home = () => {
 						</h2>
 					</div>
 				</div>
-				<div className='index_advantages'>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>01</div>
-						<div className='index_advantages_item_icon'>
+				<div className="index_advantages">
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">01</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_cool.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_cool.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>Эффективность</div>
+							<div className="index_advantages_item_title">Эффективность</div>
 						</div>
 					</div>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>02</div>
-						<div className='index_advantages_item_icon'>
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">02</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_pencil.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_pencil.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>Креативные идеи</div>
+							<div className="index_advantages_item_title">Креативные идеи</div>
 						</div>
 					</div>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>03</div>
-						<div className='index_advantages_item_icon'>
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">03</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_cubok.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_cubok.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>Оптимизация</div>
+							<div className="index_advantages_item_title">Оптимизация</div>
 						</div>
 					</div>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>04</div>
-						<div className='index_advantages_item_icon'>
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">04</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_tool.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_tool.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>Стратегия</div>
+							<div className="index_advantages_item_title">Стратегия</div>
 						</div>
 					</div>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>05</div>
-						<div className='index_advantages_item_icon'>
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">05</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_clock.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_clock.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>
+							<div className="index_advantages_item_title">
 								Работа на результат
 							</div>
 						</div>
 					</div>
-					<div className='index_advantages_item'>
-						<div className='index_advantages_item_number'>06</div>
-						<div className='index_advantages_item_icon'>
+					<div className="index_advantages_item">
+						<div className="index_advantages_item_number">06</div>
+						<div className="index_advantages_item_icon">
 							<img
-								src='/img/advantages_patern.png'
-								alt=''
-								className='index_advantages_icon'
+								src="/img/advantages_patern.png"
+								alt=""
+								className="index_advantages_icon"
 							/>
-							<div className='index_advantages_item_title'>
+							<div className="index_advantages_item_title">
 								Работаем без шаблонов
 							</div>
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className='index_why_we'>
-				<div className='wrapper'>
+			<section className="index_why_we">
+				<div className="wrapper">
 					<h2>Почему мы?</h2>
-					<div className='shift_description'>
-						{window.innerWidth >= 100 && (<><p>
-							Мы придерживаемся принципу индивидуального подхода к каждому
-							проекту, любой наш проект уникален.
-						</p>
-						<p>Мы создаем сайты для бизнеса, которые будут работать на вас.</p>
-						<p>
-							Мы всегда стремимся найти самые конкурентоспособные решения и
-							достичь самого высокого уровня эффективности по всем проектам.
-						</p>
-						<p>
-							Мы создаем landing, корпоративный сайт, интернет – магазин любой
-							сложности.
-						</p>
-						<p>Мы продвигаем сайты на всех интернет- площадках.</p>
-						<p>
-								Заказать создание сайта от веб - студии International Techno
-								Dynamics, значит доверить свой имидж лучшим.
-						</p></>)}
+					<div className="shift_description">
+						{window.innerWidth >= 100 && (
+							<>
+								<p>
+									Мы придерживаемся принципу индивидуального подхода к каждому
+									проекту, любой наш проект уникален.
+								</p>
+								<p>
+									Мы создаем сайты для бизнеса, которые будут работать на вас.
+								</p>
+								<p>
+									Мы всегда стремимся найти самые конкурентоспособные решения и
+									достичь самого высокого уровня эффективности по всем проектам.
+								</p>
+								<p>
+									Мы создаем landing, корпоративный сайт, интернет – магазин
+									любой сложности.
+								</p>
+								<p>Мы продвигаем сайты на всех интернет- площадках.</p>
+								<p>
+									Заказать создание сайта от веб - студии International Techno
+									Dynamics, значит доверить свой имидж лучшим.
+								</p>
+							</>
+						)}
 						{window.innerWidth <= 100 && (
 							<ReadMore
 								text={divText.innerHTML}
@@ -238,17 +249,20 @@ const Home = () => {
 					</div>
 				</div>
 			</section>
-			<section className='index_contacts'>
-				<div className='wrapper'>
+			<section className="index_contacts">
+				<div className="wrapper">
 					<h2>
 						Оставьте заявку
 						<br />и мы с Вами свяжемся!
 					</h2>
 					<Forms />
+					<center>
+						<button className="downloadBtn">Скачать</button>
+					</center>
 				</div>
 			</section>
 			<Footer />
-			{click && ( <Clickbait setCheck={setClick} check={click} /> )}
+			{click && <Clickbait setCheck={setClick} check={click} />}
 		</>
 	)
 }
